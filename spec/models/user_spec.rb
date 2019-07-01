6edb4f07ba4f7afff7384bc12210830c30753e6e
Oscar M. Olivera <oscarmolivera@gmail.com>
2019-07-01 19:27:29 -0400

    Pruebas de validacion de Usarios

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.create!(email: 'userTest@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', first_name:'Jon', last_name:'Snow')
+  end
+  
+  it "user can be created" do
+    expect(@user).to be_valid
+  end
+
+  it "user can NOT be created" do
+    @user.first_name = nil
+    @user.last_name = nil
+    expect(@user).to_not be_valid
+  end 
+  
 end
