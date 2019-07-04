91ebe6400c546e6fd5a6b0480bdd8ef3db10d6ac
Oscar M. Olivera <oscarmolivera@gmail.com>
2019-07-04 18:02:08 -0400

    Inclusion de Navbar con enlaces a paginas y cierre de session

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-=begin
+User.create!(email: "oscarmolivera@gmail.com", first_name: "Oscar", last_name: "Olivera", password: "147852", password_confirmation: "147852")
+
 100.times do |post|
-  Post.create!(date: Date.today, rationale:"#{post} It is a Comment POst!")
+  Post.create!(date: Date.today, rationale:"#{post} It is a Comment POst!", user_id: 1)
 end
 puts "Creados 100 Posts!"
-=end
