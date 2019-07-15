4448b85784d1b85ac229892c0af664ba256d1e41
Oscar M. Olivera <oscarmolivera@gmail.com>
2019-07-15 09:27:57 -0400

    Integracion de permisos a pasos de procesos en Post

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -11,11 +11,20 @@ describe 'navigate' do
       @post = FactoryBot.create(:post)
     end
 
-    it 'has a status that can be edited on the form' do
+    it 'has a status that can be edited on the form by an Admin' do
       visit edit_post_path(@post)
       choose 'post_status_aprobado'
       click_on "Save"
       expect(@post.reload.status).to eq('aprobado')
+    end
+
+    it 'can not be edited by a non admin' do
+      logout(@admin_user)
+      user = FactoryBot.create(:user)
+      login_as(user, scope: :user)
+
+      visit edit_post_path(@post)
+      expect(page).to_not have_content('Aprobado')
     end
   end
 end
