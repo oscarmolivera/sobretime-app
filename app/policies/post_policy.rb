42d9cb52bcf826039bfcfe0a9e4894f29524e0a2
Oscar M. Olivera <oscarmolivera@gmail.com>
2019-07-16 16:02:40 -0400

    Integrando una politica para los POST usando PUNDIT

---
+++ app/policies/post_policy.rb
@@ -0,0 +1,5 @@
+class PostPolicy < ApplicationPolicy
+  def update?
+    record.user.id == user.id || admin_types.include?(user.type)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
-User.create!(email: "oscarmolivera@gmail.com", first_name: "Oscar", last_name: "Olivera", password: "147852", password_confirmation: "147852")
-User.create!(email: "newadmin@mysite.com", first_name: "Admin", last_name: "User", password: "147852", password_confirmation: "147852", type: :AdminUser)
-100.times do |post|
+User.create!(email: "oscarmolivera@gmail.com", first_name: "Oscar", last_name: "Olivera", password: "123456", password_confirmation: "123456")
+User.create!(email: "usuario2@gmail.com", first_name: "Normal", last_name: "User", password: "123456", password_confirmation: "123456")
+User.create!(email: "newadmin@mysite.com", first_name: "Admin", last_name: "User", password: "123456", password_confirmation: "123456", type: :AdminUser)
+20.times do |post|
   Post.create!(date: Date.today, rationale:"#{post} It is a Comment POst!", user_id: 1)
 end
-puts "Creados 100 Posts!"
+20.times do |post|
+  Post.create!(date: Date.today, rationale:"#{post} It is a Comment POst!", user_id: 2)
+end
+10.times do |post|
+  Post.create!(date: Date.today, rationale:"#{post} It is a Comment POst!", user_id: 3)
+end
+puts "Creados 60 Posts!"
