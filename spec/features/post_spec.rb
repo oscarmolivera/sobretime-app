d01e003ea2669bf70f6f2b4f81950252d25cc315
Oscar M. Olivera <oscarmolivera@gmail.com>
2019-07-16 23:12:42 -0400

    Permisos de usuarios y lista de post + Pruebas

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -19,8 +19,20 @@ describe 'navegate' do
     it 'has some Posts with content' do
       post1 = FactoryBot.create(:post)
       post2 = FactoryBot.create(:second_post)
+      post1.update(user_id: @user.id)
+      post2.update(user_id: @user.id)
       visit posts_path
       expect(page).to have_content(/Some|rationale/)
+    end
+
+    it 'tiene solo la lista de POST creados por el usuario' do
+      post1 =  Post.create!(date: Date.today, rationale:"Test rationale Numero Uno!", user_id: @user.id)
+      post2 =  Post.create!(date: Date.today, rationale:"Test rationale Numero Uno!", user_id: @user.id)
+      other_user = User.create!(email: "user@test.com", first_name: "Test", last_name: "User", password: "123456", password_confirmation: "123456")
+      other_user_post =  Post.create!(date: Date.today, rationale:"Test rationale Numero DOS!", user_id: other_user.id)
+      #byebug
+      visit posts_path
+      expect(page).to_not have_content(/Test rationale Numero DOS!/)
     end
 
     it 'has a user with a Last and First Name' do
@@ -42,9 +54,10 @@ describe 'navegate' do
 
   describe 'delete' do
      it 'y existe un enlace para borrar Post' do
-      @post = FactoryBot.create(:post)
+      post = FactoryBot.create(:post)
+      post.update(user_id: @user.id)
       visit posts_path
-      click_link("delete#{@post.id}")
+      click_link("delete#{post.id}")
       expect(page.status_code).to eq(200)
     end
   end
