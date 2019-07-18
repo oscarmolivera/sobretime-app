24d08c0bd5274b8512db8d2210fa496199664177
Oscar M. Olivera <oscarmolivera@gmail.com>
2019-07-18 10:01:00 -0400

    Helper para Etiqeuta de estado en Lista posts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,17 @@
 module PostsHelper
+  def status_label status
+    status_label_generator status
+  end
+
+  private
+    def status_label_generator status
+      case status
+      when 'registrado'
+        content_tag(:span, status.titleize, class: 'label label-primary')
+      when 'aprobado'
+        content_tag(:span, status.titleize, class: 'label label-success')
+      when 'rechazado'
+        content_tag(:span, status.titleize, class: 'label label-danger')          
+      end
+    end
 end
