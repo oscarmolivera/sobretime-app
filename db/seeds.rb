d65f9ff7c91268e502cc8fb766a41cff0218a894
Oscar M. Olivera <oscarmolivera@gmail.com>
2019-07-22 19:43:09 -0400

    Config de FactoryBot + Nuevos Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,23 @@
-User.create!(email: "oscarmolivera@gmail.com", first_name: "Oscar", last_name: "Olivera", password: "123456", password_confirmation: "123456", phone: "56947880067")
-User.create!(email: "usuario2@gmail.com", first_name: "Normal", last_name: "User", password: "123456", password_confirmation: "123456", phone: "56978663322")
-AdminUser.create!(email: "newadmin@mysite.com", first_name: "Admin", last_name: "User", password: "123456", password_confirmation: "123456", phone: "56947880067")
+User.create!(email: "oscarmolivera@gmail.com", first_name: "Oscar", last_name: "Olivera", password: "123456", password_confirmation: "123456", phone: "6947880067")
+User.create!(email: "usuario2@gmail.com", first_name: "Normal", last_name: "User", password: "123456", password_confirmation: "123456", phone: "6978663322")
+AdminUser.create!(email: "newadmin@mysite.com", first_name: "Admin", last_name: "User", password: "123456", password_confirmation: "123456", phone: "6947880067")
 20.times do |post|
   Post.create!(date: Date.today, rationale:"#{post+1} It is a Comment Post from Oscar Olivera!", user_id: 1, overtime_request: 1.0)
+end
+20.times do |audit_log|
+  AuditLog.create!(user_id: 1, status: 0, start_date: ((Date.today + audit_log.days) -6.days ))
 end
 20.times do |post|
   Post.create!(date: Date.today, rationale:"#{post+1} It is a Comment Post From Normal User!", user_id: 2, overtime_request: 1.3)
 end
+20.times do |audit_log|
+  AuditLog.create!(user_id: 2, status: 0, start_date: ((Date.today + audit_log.days) -6.days ))
+end
 10.times do |post|
   Post.create!(date: Date.today, rationale:"#{post+1} It is a Comment Post from ADMIN User!", user_id: 3, overtime_request: 0.5)
 end
-puts "Creados 60 Posts!"
+10.times do |audit_log|
+  AuditLog.create!(user_id: 3, status: 0, start_date: ((Date.today + audit_log.days) -6.days ))
+end
+puts "Creados 50 Posts!"
+puts "Creados 50 Auditorias!"
--- spec/factories/audit_logs.rb
+++ spec/factories/audit_logs.rb
@@ -1,8 +1,8 @@
-FactoryBot.define do
-  factory :audit_log do
-    user { nil }
-    status { 1 }
-    start_date { "2019-07-22" }
-    end_date { "2019-07-22" }
+FactoryBot.define do 
+  factory :audit_log do |al|
+    al.user
+    al.status { 0 }
+    al.start_date { Date.today - 6.days}
+    al.end_date { nil }
   end
 end
