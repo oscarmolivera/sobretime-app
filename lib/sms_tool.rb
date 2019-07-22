bcbea027e40595767cdffa4b5abc0f48b127073d
Oscar M. Olivera <oscarmolivera@gmail.com>
2019-07-22 17:17:56 -0400

    Funcionalidad de Twilio con adaptacion a version Actual

--- lib/sms_tool.rb
+++ lib/sms_tool.rb
@@ -1,6 +1,19 @@
 module SmsTool
-  def self.send_sms(number:, message: )
-    puts "Sending SMS ..."
-    puts "#{message} to #{number}"
-  end
-end 
+    ACCOUNT_SID = Rails.application.credentials.twilio[:account_sid]
+    AUTH_TOKEN  = Rails.application.credentials.twilio[:auth_token]
+    TRIAL_PHONE = Rails.application.credentials.twilio[:trial_phone]
+
+    @client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
+
+    def self.send_sms(number:, message:)
+      byebug
+      @client.api.account.messages.create(
+        to: "+56#{number}", 
+        from: TRIAL_PHONE,
+        body: "#{message}"
+      )
+    end
+
+end   
+
+#SmsTool.send_sms(number: "947880067", message:"First Text From Twilio")
