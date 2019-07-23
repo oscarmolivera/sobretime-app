42c0b4a585c4bc95be17df3df1c070eb50094b01
Oscar M. Olivera <oscarmolivera@gmail.com>
2019-07-23 11:55:04 -0400

    AuditLog Tests /  Validations en el Modelo

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,3 +1,15 @@
 class AuditLog < ApplicationRecord
   belongs_to :user
+  
+  after_initialize :set_default
+
+  validates :user,  presence: true
+  validates :status,  presence: true
+  validates :start_date,  presence: true
+
+  private 
+    def set_default
+      self.start_date ||= Date.today - 6.days
+    end
+  
 end
