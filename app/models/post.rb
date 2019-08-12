f543465cbb71ff854de65a711a4251aefec55cd0
Oscar M. Olivera <oscarmolivera@gmail.com>
2019-08-12 14:11:32 -0400

    Call back para Modificar AuditLog cuando se ingresen POST de la semana

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,12 @@ class Post < ApplicationRecord
   validates :overtime_request, numericality: { greater_than: 0 }
 
   scope :posts_by, lambda{|user| where user_id: user.id}
+
+  after_save :update_audit_log
+
+  private
+    def update_audit_log
+      audit_log = AuditLog.where(user_id: self.user.id, start_date: (self.date - 7.days..self.date)).last
+      audit_log.confirmado!
+    end
 end
