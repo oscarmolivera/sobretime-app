8ffbc183b35c7da5b4942f523c509f55a16ec38c
Oscar M. Olivera <oscarmolivera@gmail.com>
2019-08-17 19:42:16 -0400

    Nuevo colback para incluir fecha de confirmacion AuditLog

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -3,14 +3,20 @@ class AuditLog < ApplicationRecord
   belongs_to :user
   
   after_initialize :set_default
+  before_save :set_end_date if :confirmado?
 
   validates :user,  presence: true
   validates :status,  presence: true
   validates :start_date,  presence: true
 
+  default_scope { order(id: :asc) }
+
   private 
     def set_default
       self.start_date ||= Date.today - 6.days
     end
-  
+
+    def set_end_date
+      self.end_date = Date.today
+    end
 end
